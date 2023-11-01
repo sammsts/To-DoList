452a5ab0fc6bf54c0be8e354728f173f06467ff8
sammsts <samuelmacielsts@gmail.com>
2023-11-01 14:56:45 -0300

    Feature: implementing the use of localStorage and Lifecycle Hooks

--- scripts.js
+++ scripts.js
@@ -10,15 +10,27 @@ const toDoList = {
 
     methods: {
         addTask: function() {
-            if (this.newTask.message) {
+            if (this.newTask.text) {
                 this.taskList.push(this.newTask);
                 this.newTask = {
                     active: false
                 };
+                
+                localStorage.setItem("tasks", JSON.stringify(this.taskList))
+
             } else {
                 alert("To-do text is required");
             }
-        },
+        }
+    },
+
+    created() {
+        this.taskList = localStorage.getItem("tasks") ? JSON.parse(localStorage.getItem("tasks")) : this.taskList;
+    },
+
+    //Utilizado no update porque não é uma aplicação grande, então, não vai pesar o sistema.
+    updated() {
+        localStorage.setItem("tasks", JSON.stringify(this.taskList));
     }
 };
 
