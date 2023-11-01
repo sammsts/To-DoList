cc86ad883051ac492f7009d9fea1ff1f109bb117
sammsts <samuelmacielsts@gmail.com>
2023-11-01 13:54:07 -0300

    fix: logic tuning and code optimization. Hidden delete button when there are no tasks in the list

--- scripts.js
+++ scripts.js
@@ -1,40 +1,25 @@
-var taskList = [
-    {
-        "active": false,
-        "message": ""
-    },
-]
-
 const toDoList = {
     data() {
         return {
-            taskList: window.taskList,
-            newTask: {}
+            taskList: [],
+            newTask: {
+                active: false
+            }
         }
-    },
-
-    computed: {
-        filteredTaskList() {
-            return this.taskList.filter((task) => task.message.trim() !== '');
-        },
     },
 
     methods: {
         addTask: function() {
             if (this.newTask.message) {
                 this.taskList.push(this.newTask);
-                this.newTask = {};
+                this.newTask = {
+                    active: false
+                };
             } else {
-                alert("All fields must be filled");
+                alert("To-do text is required");
             }
         },
-
-        removeTask: function() {
-            if (this.taskList.length > 0) {
-                this.taskList = [{"active": false, "message": ""}];
-            }
-        }
     }
-}
+};
 
 Vue.createApp(toDoList).mount('#app');
